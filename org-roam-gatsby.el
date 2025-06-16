dd1941192736a8ea9508a10926870ad80a9c35d7
daniel <dj@danieljohannsen.de>
2025-06-16 07:31:26 +0200

    Using org-publish-project-alist while dropping tag alist

--- org-roam-gatsby.el
+++ org-roam-gatsby.el
@@ -1,31 +1,73 @@
-;;; org-roam-gatsby --- Org Roam to Gatsby
+;;; org-roam-gatsby --- Org Roam to Gatsby  -*- lexical-binding: t; -*-
 
 
 ;;; Commentary:
 
 
 ;;; Code:
+(require 'org)
+(require 'org-roam)
+(require 'ox)
 
-(defvar e/org-roam-gatsby-tags-alist
-  '(("~/.org-roam-public" "public" "/roam")
-    ("~/.org-roam-public-nv" "public-nv" "/roam")))
+(defvar e/org-roam-gatsby-plist nil)
+
 (defvar e/org-export-gatsby-header nil)
 
+(defvar e/org-roam-gatsby-hook nil)
 
-(defun e/org-roam-gatsby-sites ()
+(defun e/org-roam-gatsby-plist-find ( tag)
+  (let (( plists (mapcar 'cdr org-publish-project-alist))
+        plist)
+    (while plists
+      (setq plist (pop plists))
+      (when (string= (plist-get plist :tag) tag)
+        (setq plists nil)))
+    plist))
+
+(defun e/org-roam-gatsby-symlink-after-save ()
+  "Maintain symbolic links to publishing directory.
+
+Works with the current buffer."
   (let* (( file-name (buffer-file-name (buffer-base-buffer (current-buffer))))
          ( file-nondir (file-name-nondirectory file-name))
          ( filetags (split-string (or (cadar (org-collect-keywords '("FILETAGS"))) "")
-                                  ":" 'OMIT-NULLS))
-         link-name)
-    (dolist ( tag e/org-roam-gatsby-tags-alist)
-      (setq link-name (concat (expand-file-name (file-name-as-directory (car tag)))
-                              file-nondir))
-      (if (member (cadr tag) filetags)
-          (make-symbolic-link file-name link-name t)
-        (when (file-exists-p link-name)
-          (delete-file link-name))))))
+                                  ":" 'OMIT-NULLS)))
+    (dolist ( project org-publish-project-alist)
+      (let* (( plist (cdr project))
+             ( tag (plist-get plist :tag)))
+        (when tag
+          (let (( symbolic-link-name (expand-file-name
+                                      file-nondir
+                                      (file-name-as-directory
+                                       (plist-get plist :base-directory)))))
+            (if (member tag filetags)
+                (make-symbolic-link file-name symbolic-link-name t)
+              (when (file-exists-p symbolic-link-name)
+                (delete-file symbolic-link-name)))))))))
 
+(defun e/org-roam-gatsby-publish-after-save ()
+  (let (( filetags (split-string (or (cadar (org-collect-keywords '("FILETAGS"))) "")
+                                 ":" 'OMIT-NULLS)))
+    (when filetags
+      (dolist ( project org-publish-project-alist)
+        (let* (( plist (cdr project))
+               ( tag (plist-get plist :tag)))
+          (when (and tag (member tag filetags))
+            (message "Org-Roam-Gatsby: Publishing project \"%s\"" (car project))
+            (org-publish-project (car project))))))))
+
+(defun e/org-roam-gatsby-hook-functions ()
+  "Gatsby hook functions."
+  (add-hook 'after-save-hook #'e/org-roam-gatsby-symlink-after-save 0 'local)
+  (add-hook 'after-save-hook #'e/org-roam-gatsby-publish-after-save 10 'local))
+
+(add-hook 'e/org-roam-gatsby-hook 'e/org-roam-gatsby-hook-functions)
+
+(defun e/org-roam-gatsby-run-hook ()
+  (when (org-roam-file-p)
+    (run-hooks 'e/org-roam-gatsby-hook)))
+
+(add-hook 'org-mode-hook 'e/org-roam-gatsby-run-hook)
 
 (defun e/org-gatsby-title-slug (title)
   (replace-regexp-in-string
@@ -34,21 +76,7 @@
     "[^a-zA-Z0-9\\-$_.+!*'(),]" "-"
     (downcase title))))
 
-
-(defun e/org-roam-gatsby-site ()
-  (let (( sites e/org-roam-gatsby-tags-alist)
-        site)
-    (while sites
-      (setq site (pop sites))
-      (if (string= (expand-file-name
-                    (file-name-as-directory (car site)))
-                   e/org-publish-base-directory)
-          (setq sites nil)
-        (setq site nil)))
-    site))
-
-
-(defun e/org-translate-link (link contents info)
+(defun e/org-translate-link (link _ _)
   (let (( type (org-element-property :type link)))
     (when (string= type "id")
       (let* (( id (org-element-property :path link))
@@ -63,23 +91,21 @@
              ( title (e/org-gatsby-title-slug (car node)))
              ( file (file-name-base (cadr node)))
              ( tags (cddr node))
-             ( site (e/org-roam-gatsby-site))
-             ( tag (cadr site))
-             ( link-dir (file-name-as-directory (caddr site))))
-        (if (member tag tags)
-            (concat "<a href=\"" link-dir file "/" title "\">" description "</a>")
+             ( plist e/org-roam-gatsby-plist))
+        (if (member (plist-get plist :tag) tags)
+            (concat "<a href=\""
+                    (file-name-as-directory (plist-get plist :rel-url))
+                    file "/" title "\">" description "</a>")
           description)))))
 
-
-(defun e/org-translate-keyword (keyword contents info)
+(defun e/org-translate-keyword (keyword _ _)
   (setq e/org-export-gatsby-header
         (cons `(,(downcase (org-element-property :key keyword)) .
                 ,(org-element-property :value keyword))
               e/org-export-gatsby-header))
   nil)
 
-
-(defun e/org-translate-inner-template (text info)
+(defun e/org-translate-inner-template (text _)
   (let* (( title (assoc "title" e/org-export-gatsby-header))
          ( slug (e/org-gatsby-title-slug (cdr title))))
     (setq e/org-export-gatsby-header
@@ -90,10 +116,8 @@
     (setq e/org-export-gatsby-header nil)
     (concat header "GATSBY END -->\n" text)))
 
-
-(defun e/org-translate-property-drawer (property-drawer contents info)
+(defun e/org-translate-property-drawer (property-drawer _ _)
   (message "%s" property-drawer))
- 
 
 (org-export-define-derived-backend 'html-gatsby 'html
   :translate-alist '((property-drawer . e/org-translate-property-drawer)
@@ -101,10 +125,20 @@
                      (keyword . e/org-translate-keyword)
                      (inner-template . e/org-translate-inner-template)))
 
-  
 (defun e/org-html-publish-to-gatsby ( plist filename pub-dir)
-  (setq e/org-publish-base-directory
-        (expand-file-name (file-name-as-directory (plist-get plist :base-directory))))
+  "Publishing function for \"roam-to-gatsby\" projects.
+
+The extension of the exported file is determined by the project
+plist value for html-extension, the value of the variable
+`org-html-extension' or \"html\". The backend is html-gatsby,
+which is defined by `org-export-define-derived-backend'.
+
+PLIST is the property list for the given project,
+i.e. \"roam-to-gatsby\" projects.  FILENAME is the filename of
+the Org file to be published.  PUB-DIR, when non-nil is the
+publishing directory, i.e. the target directory where the html
+files will end up in."
+  (setq e/org-roam-gatsby-plist plist)
   (org-publish-org-to 'html-gatsby
                       filename
                       (concat "." (or (plist-get plist :html-extension)
@@ -113,7 +147,7 @@
                       plist
                       pub-dir))
 
-
+  
 
 (provide 'org-roam-gatsby)
 
